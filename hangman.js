3c7be217ddc2bff4388e39abe74fd1d0c1d5c3d3
Campbell1105 <135083465+Campbell1105@users.noreply.github.com>
2023-07-30 11:53:39 -0400

    reworked divs in css

--- hangman.js
+++ hangman.js
@@ -1,1 +1,20 @@
-//start
+
+    // const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    //       'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    //       't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        
+    //         for (var i = 0; i < alphabet.length; i++) {
+    //           letters = 'alphabet';
+    //           list = document.createElement('li');
+    //           list.id = 'letter';
+    //           list.innerHTML = alphabet[i];
+    //           myButtons.appendChild(letters);
+    //           letters.appendChild(list);
+    //         }
+    
+
+document.getElementById("clues").innerHTML= "Here are where my Clues go"
+
+document.getElementById("letters").innerHTML= "this is the keyboard area"
+
+document.getElementById("wordStatus").innerHTML= "progress on the mystery word"
