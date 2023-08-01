fbcf1d89657ead9a6bf5bd41e235023de95c2e25
Campbell1105 <135083465+Campbell1105@users.noreply.github.com>
2023-08-01 18:14:49 -0400

    added alphabet buttons, CSS & JS

--- hangman.js
+++ hangman.js
@@ -1,7 +1,37 @@
-document.getElementById("letters").innerHTML= "progress on the mystery word"
-    // const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-    //       'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    //       't', 'u', 'v', 'w', 'x', 'y', 'z'];
-        
+window.onload = function () {
 
-document.getElementById("wordStatus").innerHTML= "progress on the mystery word"
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+          'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+          't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+
+    var getHint ;          // Word getHint
+    var word ;              // Selected word
+    var guess ;             // Geuss
+    var geusses = [ ];      // Stored geusses
+    var lives ;             // Lives
+    var counter ;           // Count correct geusses
+    var space;              // Number of spaces in word '-'
+  
+    // Get elements
+    var showClue = document.getElementById("clue");
+  
+  
+  
+    // create alphabet ul
+    var buttons = function () {
+    var div = document.getElementById('alphabetButtons');
+      myButtons = document.getElementById('buttons');
+      letters = document.createElement('ul');
+  
+      for (var i = 0; i < alphabet.length; i++) {
+        letters.id = 'alphabet';
+        list = document.createElement('li');
+        list.id = 'letter';
+        list.innerHTML = alphabet[i];
+        check();
+        myButtons.appendChild(letters);
+        letters.appendChild(list);
+      }
+    }
+}
