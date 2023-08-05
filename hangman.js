a6fb307990ceb8274e259f8cef67f28b8204adbd
Campbell1105 <135083465+Campbell1105@users.noreply.github.com>
2023-08-05 11:09:20 -0400

    adding clues?

--- hangman.js
+++ hangman.js
@@ -40,12 +40,11 @@ const clueList = [
 
 const alphabet = document.querySelector(".alphabetBox");
 
-console.log(alphabetArray);
-// const buttons = function () {
 
-//     alphabet.appendChild(alphabetArray)
-//     const myButtons = document.getElementById('buttons');
-//       letters = document.createElement('ul');
+let chosenLoopedWord;
+
+let wordIndexForComparing;
+
 
 for (var i = 0; i < alphabetArray.length; i++) {
   let mainLetterNode = document.querySelector(".alphabetBox");
@@ -65,18 +64,20 @@ for (var i = 0; i < alphabetArray.length; i++) {
   let letterClick = document.getElementById(`${alphabetArray[i]}`);
 
   // let grayOut = document.getElementById()
-  letterClick.addEventListener("click", (e) => {
+  letterClick.addEventListener("click", e => {
     console.log(e);
     // gray out the letter chosen,
     e.target.style.color = "rgb(46, 12, 12)";
 
-    // if (letter.clicked){
-    //   console.log("clicked")
-    //   //if the div has been clicked, then change the style to gray
-    //   letterDiv.style.color = "lightgrey";
-    // }
+    
+ 
   });
+
+  // document.getElementById("wordStatus").style.visibility = "hidden";
+  //   letterClick.addEventListener("click", e => {e.target.style.visibility = "visible" 
+  // })
 }
+
 
 //__________________________________________________________________________________________
 
@@ -85,29 +86,61 @@ let answer = "";
 function setAnswer() {
   //randomly choose a word to show
 
-  const wordOrder = wordList[Math.floor(Math.random() * wordList.length)];
+  const orderedWord = wordList[Math.floor(Math.random() * wordList.length)];
   const chosenWord = document.getElementById("wordStatus");
-  chosenWord.innerHTML = wordOrder; 
-  const answer = chosenWord;
-  const wordIndex = wordList.indexOf(wordOrder);
-  console.log(wordIndex);
+  chosenWord.innerHTML = orderedWord; 
+  // const answer = chosenWord;
+  const wordIndex = wordList.indexOf(orderedWord);
+  // console.log(wordIndex);
+  indexedWordForComparing = wordList[wordIndex]
+  
+  console.log(indexedWordForComparing.split(""))
+// console.log(wordIndexForComparing)
 
   // compare word index with the clue index
 
   // set answer elements to 'display none' and reveal when clicked???
-
+  
+  //   console.log(e);
+  //   // gray out the letter chosen,
+     
+  chosenLoopedWord = chosenWord
   //TODO: when answer is chosen randomly, the clue is also accessed and shown
 
-  // function checkArrays(answer, clueList) {
-  //   for (i = 0; i < array.length; i++)
-  //   let(answer.array[i] === clueList.array[i]);
-  //   let(clueList.array[i] = querySelector.innerHTML(".clueClass"));
-  // }
-}
-setAnswer();
-// const wordDisplay = [];
+  function checkArrays(chosenWord, clueList) {
+    for (i = 0; i < array.length; i++)
+    let(chosenWord.array[i] === clueList.array[i]);
+    let(clueList.array[i] = querySelector.innerHTML(".clueClass"));
 
-// let generatedAnswerDisplay = document.getElementById("wordStatus");
+
+  for (let i = 0; i < alphabetArray.length; i++) {
+    for (let j = 0; j < indexedWordForComparing.length; j++) {
+    // console.log(alphabetArray[i].innerHTML)
+    // console.log(indexedWordForComparing[j])
+  
+      if (alphabetArray[i] === indexedWordForComparing[j]) {
+        // console.log(indexedWordForComparing[j])
+        let answerLetterDiv = document.createElement('div')
+
+        
+        //
+      }
+  
+  
+     
+    }
+    
+  }
+
+   }
+  }
+   setAnswer();
+
+
+   // const wordDisplay = [];
+  //  console.log(chosenLoopedWord.innerHTML)
+   
+   // let generatedAnswerDisplay = document.getElementById("wordStatus");
 
 // function generateAnswerDisplay() {
 
@@ -115,49 +148,26 @@ setAnswer();
 
 //inside word status, have as many underscore images inside divs as there are words in the answer
 
-{
-  for (var i = 0; i < answer.length; i++) {
-    let mainAnswerNode = document.querySelector(".wordStatus");
+// {
+//   for (var i = 0; i < answer.length; i++) {
+//     let mainAnswerNode = document.querySelector(".wordStatus");
 
-    let blankDiv = document.createElement("div");
+//     let blankDiv = document.createElement("div");
 
-    // let mainAnswerTextNode = document.createTextNode(`${answer[i]}`);
+//     // let mainAnswerTextNode = document.createTextNode(`${answer[i]}`);
 
-    let answerNode = document.createTextNode(`${answer[i]}`);
+//     let answerNode = document.createTextNode(`${answer[i]}`);
 
-    blankDiv.appendChild(answerNode);
+//     blankDiv.appendChild(answerNode);
 
-    blankDiv.setAttribute("id", `letter-${i}`);
 
-    mainAnswerNode.appendChild(blankDiv);
 
-    // element.style.border = "white 5px solid";
-    // element.style.width = "60px";
-    // element.style.width = "100px";
-  }
-}
+//advance the hangman a step
 
-//letter clicked matches letter in answer){}
+// const containerHint = document.getElementById("clues");
 
-//replace blank id with letter id that goes according the position in the answer
+// function showClues() {
+//   containerHint.innerHTML = `clues - ${hint}`;
+// }
 
-// let splitWord = answer[i].split("")
-
-// for(var j = 0; j < splitWord.length; i++
-//create a div with an image underline in it for each letter
-// letterBlank.???
-
-//iterate over the letters of the answer
-//  if a letter matches a letter chosen by the user then the letter will replace the image with the blank as many times as it is in the answer
-
-//__________________________________________________________________________________________
-
-//advance the hangman a step}
-
-const containerHint = document.getElementById("clues");
-
-function showClues() {
-  containerHint.innerHTML = `clues - ${hint}`;
-}
-
-showClues();
+// showClues();
